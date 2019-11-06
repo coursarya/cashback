4a35e4c3130b204a04162d8fc1b6779a23795757
coursar <coursar@gmail.com>
2019-11-06 15:46:58 +0300

    fixes #2

--- src/main/java/mobi/coursar/service/CashbackService.java
+++ src/main/java/mobi/coursar/service/CashbackService.java
@@ -2,7 +2,9 @@ package mobi.coursar.service;
 
 public class CashbackService {
     public int calculate(int purchase) {
-        int cashback = purchase * 5 / 100;
-        return Math.min(cashback, 3_000);
+        int percent = 5;
+        int cashback = purchase * percent / 100;
+        int max = 3_000;
+        return Math.min(cashback, max);
     }
 }
